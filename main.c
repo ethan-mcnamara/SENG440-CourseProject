a005dd7e560ce871b1329f466824efe8bcca550f
Ethan McNamara <ethanmcnamara@uvic.ca>
2022-07-15 13:17:38 -0700

    algorithm implemented, bug still exists where some SADs are 0 unexpectantly

--- main.c
+++ main.c
@@ -2,8 +2,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <limits.h>
 
 #define SIZEOFBLOCK 16
+#define NUMFRAMES 2
+#define NUMBLOCKS 16
 
 /*
 * Struct definitions
@@ -22,14 +25,14 @@ typedef struct Block
 
 typedef struct Frame
 {
-    Block block[16][16];
-    uint16_t differences [16][16];
-    Vector vectors [16][16];
+    Block block[NUMBLOCKS][NUMBLOCKS];
+    uint32_t differences [NUMBLOCKS][NUMBLOCKS];
+    Vector vectors [NUMBLOCKS][NUMBLOCKS];
 } Frame;
 
 typedef struct Film
 {
-    Frame frame[2];
+    Frame frame[NUMFRAMES];
 } Film;
 
 
@@ -43,6 +46,13 @@ void process_frame(Frame *cur_frame, FILE *fptr)
 
     uint8_t first_iteration = 1;
 
+    for (int i = 0; i < NUMBLOCKS; ++i)
+    {
+        for (int j = 0; j < NUMBLOCKS; ++j)
+        {
+            cur_frame->differences[i][j] = UINT32_MAX;
+        }
+    }
 
     Block *cur_block = &cur_frame->block[0][0];
 
@@ -106,7 +116,7 @@ int main(int argc, char *argv[])
     //for each block in frame
     //    find most similar (smallest SAD) other block (limit search to nearby blocks)
 
-    fptr = fopen(file_name, "rb");
+    fptr = fopen("test_images/Image1.jpg", "rb");
     if(fptr == NULL)
     {
         printf("Error!");   
@@ -115,11 +125,18 @@ int main(int argc, char *argv[])
     
     
     Frame *test_frame = (Frame*)malloc(sizeof(Frame));
+    Film *test_film = (Film*) malloc(sizeof(Film));
     printf("test above process frame\n");
 
     process_frame(test_frame, fptr);
 
-    for (int i = 0; i < 16; ++i)
+    test_film->frame[0] = *test_frame;
+    fclose(fptr);
+    fptr = fopen("test_images/Image2.jpg", "rb");
+    process_frame(test_frame, fptr);
+    test_film->frame[1] = *test_frame;
+
+    /*for (int i = 0; i < 16; ++i)
     {
         for (int j = 0; j < 16; ++j)
         {
@@ -131,8 +148,72 @@ int main(int argc, char *argv[])
                 }
             }
         }
+    }*/
+
+
+    for (int i = 0; i < NUMFRAMES - 1; ++i) // every frame
+    {
+        for (int j = 0; j < NUMBLOCKS; ++j) // every row in frame (block)
+        {
+            for (int k = 0; k < NUMBLOCKS; ++k) // every column in frame (block)
+            {
+                for (int w = 0; w < NUMBLOCKS; ++w) // every block row in other frame
+                {
+                    for (int f = 0; f < NUMBLOCKS; ++f) // every block column in other frame
+                    {
+                        uint32_t temp_sad = 0;
+                        for (int s = 0; s < SIZEOFBLOCK; ++s) // every row in cur_block (cur_pixel)
+                        {
+                            for (int r = 0; r < SIZEOFBLOCK; ++r) // every column in cur_block (cur_pixel)
+                            {
+                                for (int l = 0; l < SIZEOFBLOCK; ++l) // every pixel row in other frame
+                                {
+                                    for (int t = 0; t < SIZEOFBLOCK; ++t) // every pixel column in other frame
+                                    {
+                                        int diff = test_film->frame[i].block[j][k].pixel[s][r] - test_film->frame[i + 1].block[w][f].pixel[l][t];
+                                        // printf("Block[%d][%d], difference: %d\n", j, k, diff);
+                                        if (diff < 0)
+                                        {
+                                            temp_sad -= diff;
+                                        }
+                                        else
+                                        {
+                                            temp_sad += diff;
+                                        }
+
+                                        //printf("Block[%d][%d], temp_sad: %d\n", j, k, temp_sad);
+                                    }
+
+                                }
+                            }
+                        }
+                        if (temp_sad == 0)
+                        {
+                             printf("Block[%d][%d] compared to Block[%d][%d] in other frame, diff is 0\n", j, k, w, f);
+                        }
+                        if (test_film->frame[i].differences[j][k] > temp_sad)
+                        {
+                            // printf("In if condition, temp_sad = %d, old value = %d\n", temp_sad, test_film->frame[i].differences[j][k]);
+                            test_film->frame[i].differences[j][k] = temp_sad;
+                            test_film->frame[i].vectors[j][k].x = j - w;
+                            test_film->frame[i].vectors[j][k].y = k - f;
+                        }
+                    }
+                }
+            }
+        }
     }
     
+    for (int i = 0; i < NUMBLOCKS; ++i)
+    {
+        for (int j = 0; j < NUMBLOCKS; ++j)
+        {
+            int temp_diff = test_film->frame[0].differences[i][j];
+            int temp_x = test_film->frame[0].vectors[i][j].x;
+            int temp_y = test_film->frame[0].vectors[i][j].y;
+            //printf("Block[%d][%d]: Vector: (%d, %d); Difference: %d\n", i, j, temp_x, temp_y, temp_diff);
+        }
+    }
 
     fclose(fptr);
     return 0;
