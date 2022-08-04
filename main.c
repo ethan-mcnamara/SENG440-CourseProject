9bee2983bf81471b53e38da37c4a53dcebc50096
Ethan McNamara <ethanmcnamara@uvic.ca>
2022-08-04 14:01:17 -0700

    changed test arrays

--- main.c
+++ main.c
@@ -189,10 +189,16 @@ int main(int argc, char *argv[])
                             uint8x16_t vector_ref; // declare a vector of 16 8-bit lanes
                             uint8x16_t vector_comp; // declare a vector of 16 8-bit lanes
                             printf("After declaration, before intialization\n");
-                            const uint8_t test_array [16] = test_film->frame[frame].block[block_row_ref][block_col_ref].pixel[pixel_row];
-                            vector_ref = vld1q_u8(test_array); // load the array from memory into a vector
+                            uint8_t ref_test_array [16];
+                            uint8_t comp_test_array [16];
+                            for (int i = 0; i < 16; ++i)
+                            {
+                                ref_test_array[i] = test_film->frame[frame].block[block_row_ref][block_col_ref].pixel[pixel_row][i];
+                                comp_test_array[i] = test_film->frame[frame + 1].block[block_row_comp][block_col_comp].pixel[pixel_row][i];
+                            }
+                            vector_ref = vld1q_u8(ref_test_array); // load the array from memory into a vector
                             printf("After first initalization\n");
-                            vector_comp = vld1q_u8(&test_array[8]); // load the array from memory into a vector
+                            vector_comp = vld1q_u8(comp_test_array); // load the array from memory into a vector
                             printf("After second initliazation\n");
 
                             // Perform the Absolute Differences operation:
