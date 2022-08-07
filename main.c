a84fe112eec458d1c3565feb1190ef5e22a1a3ee
TyRicard <tricard.summer@gmail.com>
2022-08-06 22:52:19 -0700

    trying something

--- main.c
+++ main.c
@@ -83,7 +83,8 @@ int main(int argc, char *argv[])
                     uint32_t temp_sad = 0;
                     uint8_t px_row;
                     for (px_row = 0; px_row < SIZEOFBLOCK; px_row++) {
-                        uint8x16_t abs_diff = vabdq_u8(Frame2[frame2br][px_row][frame2bc], Frame1[frame1br][px_row][frame1bc]);
+                        uint8x16_t temp = Frame2[frame2br][px_row][frame2bc];
+                        uint8x16_t temp2 = Frame1[frame1br][px_row][frame1bc];
                     }
 
                  }
