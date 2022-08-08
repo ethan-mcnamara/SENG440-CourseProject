269a9cc723c05ebc95343ca81e517fdbea4a484f
TyRicard <tricard.summer@gmail.com>
2022-08-07 17:56:59 -0700

    Fixing Pixel Issue

--- main.c
+++ main.c
@@ -86,7 +86,11 @@ int main(int argc, char *argv[])
 
                         uint8x16_t sad = vabdq_u8(Frame_2_Vector, Frame_1_Vector);
                         // Cannot use accumulate function
-                        vgetq_lane_u8(sad, 0);
+                        uint8_t px_i;
+                        for (px_i = 0; px_i < SIZEOFBLOCK; px_i++) {
+                            temp_sad += vgetq_lane_u8(sad, px_i);
+                        }
+
                     }
 
                  }
