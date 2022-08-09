fb79e6a965581c63a713ee9162bf196042cadd06
TyRicard <tricard.summer@gmail.com>
2022-08-08 18:52:34 -0700

    A test

--- main.c
+++ main.c
@@ -67,8 +67,6 @@ void process_frame(Frame *cur_frame, FILE *fptr)
 
     Block *cur_block = &cur_frame->block[0][0];
 
-    //fread(&cur_pixel, sizeof(uint8_t), 1, fptr);
-
     uint8_t header_counter = 1;
 
     uint8_t rm_header [54];
@@ -108,7 +106,7 @@ void process_frame(Frame *cur_frame, FILE *fptr)
         }
 
         cur_block->pixel[cur_pixel_row % SIZEOFBLOCK][cur_pixel_col % SIZEOFBLOCK] = cur_pixel;
-        //printf("%d\n", cur_pixel);
+        printf("%d ", cur_pixel);
 
         cur_pixel_col++;
 
@@ -158,20 +156,20 @@ int main(int argc, char *argv[])
     process_frame(test_frame, fptr);
     test_film->frame[1] = *test_frame;
 
-    for (int i = 0; i < 16; ++i)
-    {
-        for (int j = 0; j < 16; ++j)
-        {
-            for (int k = 0; k < 16; ++k)
-            {
-                for (int t = 0; t < 16; ++t)
-                {
-                    printf("%3d ", test_film->frame[0].block[i][j].pixel[k][t]);
-                }
-                printf("\n");
-            }
-        }
-    }
+    // for (int i = 0; i < 16; ++i)
+    // {
+    //     for (int j = 0; j < 16; ++j)
+    //     {
+    //         for (int k = 0; k < 16; ++k)
+    //         {
+    //             for (int t = 0; t < 16; ++t)
+    //             {
+    //                 printf("%3d ", test_film->frame[0].block[i][j].pixel[k][t]);
+    //             }
+    //             printf("\n");
+    //         }
+    //     }
+    // }
 
 
     for (uint8_t frame = 0; frame < NUMFRAMES - 1; ++frame) // every frame
