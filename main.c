0e8bf275ad565bf28eb6d4e2dd56fe65362b3509
TyRicard <tricard.summer@gmail.com>
2022-08-08 19:05:14 -0700

    test

--- main.c
+++ main.c
@@ -155,19 +155,20 @@ int main(int argc, char *argv[])
     process_frame(test_frame, fptr);
     test_film->frame[1] = *test_frame;
 
-
+    for (int i = 0; i < 16; ++i)
+    {
         for (int j = 0; j < 16; ++j)
         {
             for (int k = 0; k < 16; ++k)
             {
                 for (int t = 0; t < 16; ++t)
                 {
-                    printf("%d ", test_film->frame[0].block[0][j].pixel[0][t]);
-                }
+                    printf("%3d ", test_film->frame[0].block[i][j].pixel[k][t]);
+        }
 
-          }
-   }
- 
+            }
+        }
+ }
 
 
     for (uint8_t frame = 0; frame < NUMFRAMES - 1; ++frame) // every frame
