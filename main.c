8e4cac3544996941579c71bc7cf416520e357426
TyRicard <tricard.summer@gmail.com>
2022-08-09 21:31:29 -0700

    Making a parameter be a pointer to correctly pass values

--- main.c
+++ main.c
@@ -31,8 +31,8 @@ typedef struct Vector
 * Function definitions
 */
 
-void process_frame(uint8x16_t*** restrict Frame1, 
-                   uint8x16_t*** restrict Frame2)
+void process_frame(uint8x16_t* restrict Frame1[SIZEOFBLOCK][SIZEOFBLOCK], 
+                   uint8x16_t* restrict Frame2[SIZEOFBLOCK][SIZEOFBLOCK])
 {
     FILE *fptr1;
     FILE *fptr2;
