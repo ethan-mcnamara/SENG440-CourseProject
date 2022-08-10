53d0badb6c84c29052ddee43a3b2e6c66ac40ddd
TyRicard <tricard.summer@gmail.com>
2022-08-09 18:45:59 -0700

    Adding t2 back

--- main.c
+++ main.c
@@ -116,11 +116,11 @@ int main(int argc, char *argv[])
     uint8_t t = NEGATIVECHECK(-1);
     printf("%d\n", t);
 
-    uint8_t t = NEGATIVECHECK(0);
-    printf("%d\n", t);
+    uint8_t t1 = NEGATIVECHECK(0);
+    printf("%d\n", t1);
 
-        uint8_t t = NEGATIVECHECK(1);
-    printf("%d\n", t);
+        uint8_t t2 = NEGATIVECHECK(1);
+    printf("%d\n", t2);
 
     // Start calculating the SAD values
     for (uint8_t block_row_ref = 0; block_row_ref < NUMBLOCKS; ++block_row_ref) // every row in frame (block)
