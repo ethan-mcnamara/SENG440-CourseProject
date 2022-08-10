68ff14e0260b97176327286026f5df3e1e78bcf8
TyRicard <tricard.summer@gmail.com>
2022-08-09 23:03:27 -0700

    Making sure code runs according to expectation

--- main.c
+++ main.c
@@ -211,7 +211,7 @@ int main(int argc, char *argv[])
     // time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
     // printf("The elapsed time is %f seconds", time_spent);
 
-/*
+
     for (int32_t i = 0; i < NUMBLOCKS; ++i)
     {
         for (int32_t j = 0; j < NUMBLOCKS; ++j)
@@ -222,6 +222,6 @@ int main(int argc, char *argv[])
             printf("Block[%d][%d]: Vector: (%d, %d); Difference: %d\n", i, j, temp_x, temp_y, temp_diff);
         }
     }
-*/
+
     return 0;
 }
