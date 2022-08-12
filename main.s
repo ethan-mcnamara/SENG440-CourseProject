dc42240fa8e4e3d36b6bfa46426b4c8de25001b6
tyricard <tricard.summer@gmail.com>
2022-08-11 17:41:40 -0700

    Redo the assembly code to account for the timer

--- main.s
+++ main.s
@@ -138,37 +138,35 @@ process_frame:
 	.fpu neon
 	.type	main, %function
 main:
-	@ args = 0, pretend = 0, frame = 132704
+	@ args = 0, pretend = 0, frame = 132712
 	@ frame_needed = 0, uses_anonymous_args = 0
 	push	{r4, r5, r6, r7, r8, r9, r10, fp, lr}
 	sub	sp, sp, #132096
-	sub	sp, sp, #620
+	sub	sp, sp, #628
+	bl	clock
 	add	r1, sp, #66560
-	add	r0, sp, #1632
-	add	r1, r1, #616
-	add	r0, r0, #8
+	str	r0, [sp, #88]
+	add	r1, r1, #624
+	add	r0, sp, #1648
 	bl	process_frame
 	mov	r3, #0
-	add	r0, sp, #1632
-	// Change made here, combinining two add instructions into one
-	add	r0, r0, #24
-	//add	r0, r0, #16
-	str	r0, [sp, #68]
+	add	r0, sp, #1664
+	str	r0, [sp, #72]
 	add	r0, sp, #66560
-	add	r0, r0, #616
-	add	r2, sp, #616
-	add	r1, sp, #104
+	add	r0, r0, #624
+	add	r2, sp, #624
+	add	r1, sp, #112
 	add	r0, r0, #16
-	str	r3, [sp, #56]
-	str	r2, [sp, #92]
-	str	r1, [sp, #88]
-	str	r0, [sp, #100]
-	str	r1, [sp, #64]
-	str	r2, [sp, #60]
+	str	r3, [sp, #60]
+	str	r2, [sp, #100]
+	str	r1, [sp, #96]
+	str	r0, [sp, #108]
+	str	r1, [sp, #68]
+	str	r2, [sp, #64]
+	str	r3, [sp, #20]
 	str	r3, [sp, #16]
-	str	r3, [sp, #12]
 .L14:
-	ldrb	r3, [sp, #56]	@ zero_extendqisi2
+	ldrb	r3, [sp, #60]	@ zero_extendqisi2
 	sub	r1, r3, #2
 	sxtb	r1, r1
 	add	r2, r3, #3
@@ -177,35 +175,35 @@ main:
 	sub	r3, r3, r1
 	uxtb	r3, r3
 	cmp	r2, #16
-	str	r3, [sp, #96]
+	str	r3, [sp, #104]
 	movlt	r3, r2
 	movge	r3, #16
-	str	r3, [sp, #80]
-	ldr	r3, [sp, #68]
-	str	r1, [sp, #84]
+	str	r3, [sp, #84]
+	ldr	r3, [sp, #72]
+	str	r1, [sp, #92]
 	add	fp, r3, #240
-	str	r3, [sp, #28]
+	str	r3, [sp, #32]
+	ldr	r3, [sp, #68]
+	str	r3, [sp, #80]
 	ldr	r3, [sp, #64]
 	str	r3, [sp, #76]
-	ldr	r3, [sp, #60]
-	str	r3, [sp, #72]
 	mov	r3, #0
-	str	r3, [sp, #52]
+	str	r3, [sp, #56]
 .L21:
-	ldr	r1, [sp, #80]
-	ldr	r0, [sp, #84]
+	ldr	r1, [sp, #84]
+	ldr	r0, [sp, #92]
 	cmp	r1, r0
-	ble	.L25
-	ldrb	r2, [sp, #52]	@ zero_extendqisi2
+	ble	.L26
+	ldrb	r2, [sp, #56]	@ zero_extendqisi2
 	add	r3, r2, #3
 	sxtb	r3, r3
 	cmp	r3, #16
 	movlt	ip, r3
-	ldr	r3, [sp, #96]
+	ldr	r3, [sp, #104]
 	movge	ip, #16
-	str	r3, [sp, #24]
+	str	r3, [sp, #28]
 	mvn	r3, #0
-	str	r3, [sp, #8]
+	str	r3, [sp, #12]
 	mov	r3, #16
 	smlabb	r0, r0, r3, ip
 	smlabb	r1, r1, r3, ip
@@ -213,31 +211,31 @@ main:
 	sxtb	r3, r3
 	bic	r3, r3, r3, asr #31
 	sub	r2, r3, r2
-	str	ip, [sp, #36]
-	str	r3, [sp, #32]
+	str	ip, [sp, #40]
+	str	r3, [sp, #36]
 	sub	r3, r3, ip
-	ldr	ip, [sp, #100]
+	ldr	ip, [sp, #108]
 	uxtb	r2, r2
 	add	r0, ip, r0, lsl #8
 	add	r1, ip, r1, lsl #8
 	lsl	r3, r3, #8
-	str	r0, [sp, #20]
-	str	r1, [sp, #40]
-	str	r2, [sp, #48]
-	str	r3, [sp, #44]
+	str	r0, [sp, #24]
+	str	r1, [sp, #44]
+	str	r2, [sp, #52]
+	str	r3, [sp, #48]
 .L20:
-	ldr	r3, [sp, #32]
-	ldr	r2, [sp, #36]
+	ldr	r3, [sp, #36]
+	ldr	r2, [sp, #40]
 	cmp	r3, r2
 	bge	.L16
-	ldr	r3, [sp, #20]
-	ldr	r2, [sp, #44]
-	ldr	r9, [sp, #48]
+	ldr	r3, [sp, #24]
+	ldr	r2, [sp, #48]
+	ldr	r9, [sp, #52]
 	add	r10, r3, r2
 .L19:
 	mov	ip, r10
 	mov	r0, #0
-	ldr	r1, [sp, #28]
+	ldr	r1, [sp, #32]
 .L17:
 	vld1.64	{d16-d17}, [r1:64]!
 	vld1.64	{d18-d19}, [ip:64]!
@@ -251,91 +249,103 @@ main:
 	vmov.u16	r5, d17[1]
 	vmov.u16	r4, d17[2]
 	vmov.u16	lr, d17[3]
-	// Changing order of the adds to improve performance.
 	add	r2, r2, r8
-	add	r3, r3, r7
-	add	r4, r4, r6
-	add	r5, r5, lr
 	add	r3, r2, r3
-	add	r4, r4, r5
-	add r3, r3, r4
+	add	r3, r3, r7
+	add	r3, r3, r6
+	add	r3, r3, r5
+	add	r3, r3, r4
+	add	r3, r3, lr
 	cmp	r1, fp
 	add	r0, r0, r3
 	bne	.L17
-	ldr	r3, [sp, #8]
-	ldr	r1, [sp, #24]
+	ldr	r3, [sp, #12]
+	ldr	r1, [sp, #28]
 	cmp	r3, r0
-	ldr	r2, [sp, #16]
+	ldr	r2, [sp, #20]
 	movhi	r2, r1
 	movhi	r3, r0
-	str	r2, [sp, #16]
-	ldr	r2, [sp, #12]
+	str	r2, [sp, #20]
+	ldr	r2, [sp, #16]
 	movhi	r2, r9
-	str	r3, [sp, #8]
-	ldr	r3, [sp, #20]
+	str	r3, [sp, #12]
+	ldr	r3, [sp, #24]
 	add	r10, r10, #256
 	add	r9, r9, #1
 	cmp	r10, r3
-	str	r2, [sp, #12]
+	str	r2, [sp, #16]
 	uxtb	r9, r9
 	bne	.L19
 .L16:
-	ldr	r3, [sp, #20]
-	ldr	r2, [sp, #40]
-	add	r3, r3, #4096
-	str	r3, [sp, #20]
-	cmp	r3, r2
 	ldr	r3, [sp, #24]
+	ldr	r2, [sp, #44]
+	add	r3, r3, #4096
+	str	r3, [sp, #24]
+	cmp	r3, r2
+	ldr	r3, [sp, #28]
 	sub	r3, r3, #1
 	uxtb	r3, r3
-	str	r3, [sp, #24]
+	str	r3, [sp, #28]
 	bne	.L20
 .L15:
-	ldr	r3, [sp, #52]
-	ldr	r2, [sp, #12]
-	add	r3, r3, #1
-	str	r3, [sp, #52]
-	cmp	r3, #16
-	ldr	r3, [sp, #76]
-	ldr	r1, [sp, #8]
-	strb	r2, [r3]
-	ldr	r2, [sp, #16]
-	add	r3, r3, #2
-	strb	r2, [r3, #-1]
-	ldr	r2, [sp, #72]
-	str	r3, [sp, #76]
-	ldr	r3, [sp, #28]
-	str	r1, [r2], #4
-	add	r3, r3, #256
-	str	r2, [sp, #72]
-	str	r3, [sp, #28]
-	add	fp, fp, #256
-	bne	.L21
 	ldr	r3, [sp, #56]
+	ldr	r2, [sp, #16]
 	add	r3, r3, #1
 	str	r3, [sp, #56]
 	cmp	r3, #16
+	ldr	r3, [sp, #80]
+	ldr	r1, [sp, #12]
+	strb	r2, [r3]
+	ldr	r2, [sp, #20]
+	add	r3, r3, #2
+	strb	r2, [r3, #-1]
+	ldr	r2, [sp, #76]
+	str	r3, [sp, #80]
+	ldr	r3, [sp, #32]
+	str	r1, [r2], #4
+	add	r3, r3, #256
+	str	r2, [sp, #76]
+	str	r3, [sp, #32]
+	add	fp, fp, #256
+	bne	.L21
 	ldr	r3, [sp, #60]
-	add	r3, r3, #64
+	add	r3, r3, #1
 	str	r3, [sp, #60]
+	cmp	r3, #16
 	ldr	r3, [sp, #64]
-	add	r3, r3, #32
+	add	r3, r3, #64
 	str	r3, [sp, #64]
 	ldr	r3, [sp, #68]
-	add	r3, r3, #4096
+	add	r3, r3, #32
 	str	r3, [sp, #68]
+	ldr	r3, [sp, #72]
+	add	r3, r3, #4096
+	str	r3, [sp, #72]
 	bne	.L14
-	movw	r7, #:lower16:.LC4
+	bl	clock
+	ldr	r3, [sp, #88]
+	vldr.64	d19, .L34
+	sub	r0, r0, r3
+	vmov	s15, r0	@ int
+	vcvt.f64.s32	d16, s15
+	vmov.i64	d17, #0	@ float
+	vdiv.f64	d18, d16, d19
+	movw	r0, #:lower16:.LC4
+	vadd.f64	d16, d18, d17
+	movw	r7, #:lower16:.LC5
 	mov	r6, #0
-	ldr	r8, [sp, #92]
-	ldr	r10, [sp, #88]
+	vmov	r2, r3, d16
+	movt	r0, #:upper16:.LC4
+	bl	printf
+	ldr	r8, [sp, #100]
+	ldr	r10, [sp, #96]
+	movt	r7, #:upper16:.LC5
 	add	r9, r8, #1024
-	movt	r7, #:upper16:.LC4
-.L22:
+.L23:
 	mov	r5, r10
 	mov	fp, r8
 	mov	r4, #0
-.L23:
+.L24:
 	ldrsb	r3, [r5, #1]
 	ldr	r2, [fp], #4
 	str	r3, [sp]
@@ -348,21 +358,26 @@ main:
 	bl	printf
 	cmp	r4, #16
 	add	r5, r5, #2
-	bne	.L23
+	bne	.L24
 	add	r8, r8, #64
 	cmp	r9, r8
 	add	r6, r6, #1
 	add	r10, r10, #32
-	bne	.L22
+	bne	.L23
 	mov	r0, #0
 	add	sp, sp, #132096
-	add	sp, sp, #620
+	add	sp, sp, #628
 	@ sp needed
 	pop	{r4, r5, r6, r7, r8, r9, r10, fp, pc}
-.L25:
+.L26:
 	mvn	r3, #0
-	str	r3, [sp, #8]
+	str	r3, [sp, #12]
 	b	.L15
+.L35:
+	.align	3
+.L34:
+	.word	0
+	.word	1093567616
 	.size	main, .-main
 	.section	.rodata.str1.4,"aMS",%progbits,1
 	.align	2
@@ -379,6 +394,8 @@ main:
 	.ascii	"Error!\000"
 	.space	1
 .LC4:
+	.ascii	"The elapsed time is %f seconds\012\000"
+.LC5:
 	.ascii	"Block[%d][%d]: Vector: (%d, %d); Difference: %d\012"
 	.ascii	"\000"
 	.ident	"GCC: (GNU) 8.2.1 20180801 (Red Hat 8.2.1-2)"
