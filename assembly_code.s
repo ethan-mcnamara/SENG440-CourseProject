876b899a2fa9c46eb45a81155a95ecc69033efc7
tyricard <tricard.summer@gmail.com>
2022-08-12 11:06:33 -0700

    Changing the Assembly Code

--- assembly_code.s
+++ assembly_code.s
@@ -212,18 +212,18 @@ process_frame:
 	.fpu neon
 	.type	main, %function
 main:
-	@ args = 0, pretend = 0, frame = 132776
+	@ args = 0, pretend = 0, frame = 132856
 	@ frame_needed = 1, uses_anonymous_args = 0
 	push	{r4, r5, fp, lr}
 	add	fp, sp, #12
 	sub	sp, sp, #132096
-	sub	sp, sp, #688
+	sub	sp, sp, #768
 	sub	r3, fp, #131072
 	sub	r3, r3, #12
-	str	r0, [r3, #-1700]
+	str	r0, [r3, #-1780]
 	sub	r3, fp, #131072
 	sub	r3, r3, #12
-	str	r1, [r3, #-1704]
+	str	r1, [r3, #-1784]
 	mov	r2, #0
 	mov	r3, #0
 	strd	r2, [fp, #-52]
@@ -231,10 +231,10 @@ main:
 	str	r0, [fp, #-56]
 	sub	r2, fp, #131072
 	sub	r2, r2, #12
-	sub	r2, r2, #160
+	sub	r2, r2, #240
 	sub	r3, fp, #65536
 	sub	r3, r3, #12
-	sub	r3, r3, #160
+	sub	r3, r3, #240
 	mov	r1, r2
 	mov	r0, r3
 	bl	process_frame
@@ -247,11 +247,11 @@ main:
 	mov	r3, #0
 	str	r3, [fp, #-20]
 	b	.L16
-.L30:
+.L34:
 	mov	r3, #0
 	str	r3, [fp, #-24]
 	b	.L17
-.L29:
+.L33:
 	ldr	r3, [fp, #-20]
 	uxtb	r3, r3
 	sub	r3, r3, #2
@@ -262,7 +262,7 @@ main:
 	mov	r3, r0
 	str	r3, [fp, #-28]
 	b	.L18
-.L28:
+.L32:
 	ldr	r3, [fp, #-24]
 	uxtb	r3, r3
 	sub	r3, r3, #2
@@ -273,79 +273,137 @@ main:
 	mov	r3, r0
 	str	r3, [fp, #-32]
 	b	.L19
-.L27:
+.L31:
 	mov	r4, #0
 	mov	r3, #1
 	str	r3, [fp, #-36]
 	b	.L20
-.L25:
-	vldr	d16, .L36
-	vldr	d17, .L36+8
+.L29:
+	vldr	d16, .L40
+	vldr	d17, .L40+8
 	vstr	d16, [fp, #-76]
 	vstr	d17, [fp, #-68]
+	vldr	d16, .L40
+	vldr	d17, .L40+8
+	vstr	d16, [fp, #-92]
+	vstr	d17, [fp, #-84]
 	vldr	d16, [fp, #-76]
 	vldr	d17, [fp, #-68]
-	vstr	d16, [fp, #-172]
-	vstr	d17, [fp, #-164]
-	vldr	d16, [fp, #-172]
-	vldr	d17, [fp, #-164]
+	vstr	d16, [fp, #-252]
+	vstr	d17, [fp, #-244]
+	vldr	d16, [fp, #-252]
+	vldr	d17, [fp, #-244]
 	vmov.32	r3, d16[0]
-	str	r3, [fp, #-80]
-	vldr	d16, [fp, #-76]
-	vldr	d17, [fp, #-68]
-	vstr	d16, [fp, #-156]
-	vstr	d17, [fp, #-148]
-	vldr	d16, [fp, #-156]
-	vldr	d17, [fp, #-148]
-	vmov.32	r3, d16[1]
-	str	r3, [fp, #-84]
-	ldr	r3, [fp, #-80]
-	ldr	r2, [fp, #-84]
+	str	r3, [fp, #-96]
+	vldr	d16, [fp, #-92]
+	vldr	d17, [fp, #-84]
+	vstr	d16, [fp, #-236]
+	vstr	d17, [fp, #-228]
+	vldr	d16, [fp, #-236]
+	vldr	d17, [fp, #-228]
+	vmov.32	r3, d16[0]
+	str	r3, [fp, #-100]
+	ldr	r3, [fp, #-96]
+	ldr	r2, [fp, #-100]
 	.syntax divided
 @ 158 "main.c" 1
 	Manual_SAD r3 r3 r2
 @ 0 "" 2
 	.arm
 	.syntax unified
-	str	r3, [fp, #-88]
+	str	r3, [fp, #-104]
+	ldr	r3, [fp, #-104]
+	add	r4, r4, r3
 	vldr	d16, [fp, #-76]
 	vldr	d17, [fp, #-68]
-	vstr	d16, [fp, #-140]
-	vstr	d17, [fp, #-132]
-	vldr	d16, [fp, #-140]
-	vldr	d17, [fp, #-132]
-	vmov.32	r3, d17[0]
-	str	r3, [fp, #-80]
-	vldr	d16, [fp, #-76]
-	vldr	d17, [fp, #-68]
-	vstr	d16, [fp, #-124]
-	vstr	d17, [fp, #-116]
-	vldr	d16, [fp, #-124]
-	vldr	d17, [fp, #-116]
-	vmov.32	r3, d17[1]
-	str	r3, [fp, #-84]
-	ldr	r3, [fp, #-80]
-	ldr	r2, [fp, #-84]
+	vstr	d16, [fp, #-220]
+	vstr	d17, [fp, #-212]
+	vldr	d16, [fp, #-220]
+	vldr	d17, [fp, #-212]
+	vmov.32	r3, d16[1]
+	str	r3, [fp, #-96]
+	vldr	d16, [fp, #-92]
+	vldr	d17, [fp, #-84]
+	vstr	d16, [fp, #-204]
+	vstr	d17, [fp, #-196]
+	vldr	d16, [fp, #-204]
+	vldr	d17, [fp, #-196]
+	vmov.32	r3, d16[1]
+	str	r3, [fp, #-100]
+	ldr	r3, [fp, #-96]
+	ldr	r2, [fp, #-100]
 	.syntax divided
-@ 163 "main.c" 1
+@ 165 "main.c" 1
 	Manual_SAD r3 r3 r2
 @ 0 "" 2
 	.arm
 	.syntax unified
-	str	r3, [fp, #-92]
-	ldr	r3, [fp, #-88]
-	add	r2, r4, r3
-	ldr	r3, [fp, #-92]
-	add	r4, r2, r3
+	str	r3, [fp, #-104]
+	ldr	r3, [fp, #-104]
+	add	r4, r4, r3
+	vldr	d16, [fp, #-76]
+	vldr	d17, [fp, #-68]
+	vstr	d16, [fp, #-188]
+	vstr	d17, [fp, #-180]
+	vldr	d16, [fp, #-188]
+	vldr	d17, [fp, #-180]
+	vmov.32	r3, d17[0]
+	str	r3, [fp, #-96]
+	vldr	d16, [fp, #-92]
+	vldr	d17, [fp, #-84]
+	vstr	d16, [fp, #-172]
+	vstr	d17, [fp, #-164]
+	vldr	d16, [fp, #-172]
+	vldr	d17, [fp, #-164]
+	vmov.32	r3, d17[0]
+	str	r3, [fp, #-100]
+	ldr	r3, [fp, #-96]
+	ldr	r2, [fp, #-100]
+	.syntax divided
+@ 172 "main.c" 1
+	Manual_SAD r3 r3 r2
+@ 0 "" 2
+	.arm
+	.syntax unified
+	str	r3, [fp, #-104]
+	ldr	r3, [fp, #-104]
+	add	r4, r4, r3
+	vldr	d16, [fp, #-76]
+	vldr	d17, [fp, #-68]
+	vstr	d16, [fp, #-156]
+	vstr	d17, [fp, #-148]
+	vldr	d16, [fp, #-156]
+	vldr	d17, [fp, #-148]
+	vmov.32	r3, d17[1]
+	str	r3, [fp, #-96]
+	vldr	d16, [fp, #-92]
+	vldr	d17, [fp, #-84]
+	vstr	d16, [fp, #-140]
+	vstr	d17, [fp, #-132]
+	vldr	d16, [fp, #-140]
+	vldr	d17, [fp, #-132]
+	vmov.32	r3, d17[1]
+	str	r3, [fp, #-100]
+	ldr	r3, [fp, #-96]
+	ldr	r2, [fp, #-100]
+	.syntax divided
+@ 179 "main.c" 1
+	Manual_SAD r3 r3 r2
+@ 0 "" 2
+	.arm
+	.syntax unified
+	str	r3, [fp, #-104]
+	ldr	r3, [fp, #-104]
+	add	r4, r4, r3
 	ldr	r3, [fp, #-36]
 	add	r3, r3, #1
 	str	r3, [fp, #-36]
 .L20:
 	ldr	r3, [fp, #-36]
 	cmp	r3, #15
-	ble	.L25
+	ble	.L29
 	cmp	r5, r4
-	bls	.L26
+	bls	.L30
 	mov	r5, r4
 	ldr	r3, [fp, #-32]
 	uxtb	r2, r3
@@ -359,7 +417,7 @@ main:
 	uxtb	r3, r3
 	sub	r3, r2, r3
 	strb	r3, [fp, #-14]
-.L26:
+.L30:
 	ldr	r3, [fp, #-32]
 	add	r3, r3, #1
 	str	r3, [fp, #-32]
@@ -375,7 +433,7 @@ main:
 	mov	r2, r3
 	ldr	r3, [fp, #-32]
 	cmp	r3, r2
-	blt	.L27
+	blt	.L31
 	ldr	r3, [fp, #-28]
 	add	r3, r3, #1
 	str	r3, [fp, #-28]
@@ -391,7 +449,7 @@ main:
 	mov	r2, r3
 	ldr	r3, [fp, #-28]
 	cmp	r3, r2
-	blt	.L28
+	blt	.L32
 	sub	r3, fp, #131072
 	sub	r3, r3, #12
 	mov	r1, r3
@@ -401,7 +459,7 @@ main:
 	add	r3, r2, r3
 	lsl	r3, r3, #2
 	add	r3, r1, r3
-	str	r5, [r3, #-1184]
+	str	r5, [r3, #-1264]
 	ldrsb	r1, [fp, #-13]
 	sub	r3, fp, #131072
 	sub	r3, r3, #12
@@ -413,7 +471,7 @@ main:
 	lsl	r3, r3, #1
 	add	r3, r0, r3
 	mov	r2, r1
-	strb	r2, [r3, #-1696]
+	strb	r2, [r3, #-1776]
 	ldrsb	r1, [fp, #-14]
 	sub	r3, fp, #131072
 	sub	r3, r3, #12
@@ -425,7 +483,7 @@ main:
 	lsl	r3, r3, #1
 	add	r3, r0, r3
 	mov	r2, r1
-	strb	r2, [r3, #-1695]
+	strb	r2, [r3, #-1775]
 	mvn	r5, #0
 	ldr	r3, [fp, #-24]
 	add	r3, r3, #1
@@ -433,22 +491,22 @@ main:
 .L17:
 	ldr	r3, [fp, #-24]
 	cmp	r3, #15
-	ble	.L29
+	ble	.L33
 	ldr	r3, [fp, #-20]
 	add	r3, r3, #1
 	str	r3, [fp, #-20]
 .L16:
 	ldr	r3, [fp, #-20]
 	cmp	r3, #15
-	ble	.L30
+	ble	.L34
 	bl	clock
-	str	r0, [fp, #-96]
-	ldr	r2, [fp, #-96]
+	str	r0, [fp, #-108]
+	ldr	r2, [fp, #-108]
 	ldr	r3, [fp, #-56]
 	sub	r3, r2, r3
 	vmov	s15, r3	@ int
 	vcvt.f64.s32	d17, s15
-	vldr.64	d18, .L36+16
+	vldr.64	d18, .L40+16
 	vdiv.f64	d16, d17, d18
 	vldr.64	d17, [fp, #-52]
 	vadd.f64	d16, d17, d16
@@ -459,12 +517,21 @@ main:
 	bl	printf
 	mov	r3, #0
 	str	r3, [fp, #-40]
-	b	.L31
-.L34:
+	b	.L35
+.L38:
 	mov	r3, #0
 	str	r3, [fp, #-44]
-	b	.L32
-.L33:
+	b	.L36
+.L41:
+	.align	3
+.L40:
+	.word	0
+	.word	1
+	.word	2
+	.word	3
+	.word	0
+	.word	1093567616
+.L37:
 	sub	r3, fp, #131072
 	sub	r3, r3, #12
 	mov	r1, r3
@@ -474,8 +541,8 @@ main:
 	add	r3, r2, r3
 	lsl	r3, r3, #2
 	add	r3, r1, r3
-	ldr	r3, [r3, #-1184]
-	str	r3, [fp, #-100]
+	ldr	r3, [r3, #-1264]
+	str	r3, [fp, #-112]
 	sub	r3, fp, #131072
 	sub	r3, r3, #12
 	mov	r1, r3
@@ -485,9 +552,9 @@ main:
 	add	r3, r2, r3
 	lsl	r3, r3, #1
 	add	r3, r1, r3
-	sub	r3, r3, #1696
+	sub	r3, r3, #1776
 	ldrsb	r3, [r3]
-	str	r3, [fp, #-104]
+	str	r3, [fp, #-116]
 	sub	r3, fp, #131072
 	sub	r3, r3, #12
 	mov	r1, r3
@@ -497,15 +564,15 @@ main:
 	add	r3, r2, r3
 	lsl	r3, r3, #1
 	add	r3, r1, r3
-	sub	r3, r3, #1680
+	sub	r3, r3, #1760
 	sub	r3, r3, #15
 	ldrsb	r3, [r3]
-	str	r3, [fp, #-108]
-	ldr	r3, [fp, #-100]
+	str	r3, [fp, #-120]
+	ldr	r3, [fp, #-112]
 	str	r3, [sp, #4]
-	ldr	r3, [fp, #-108]
+	ldr	r3, [fp, #-120]
 	str	r3, [sp]
-	ldr	r3, [fp, #-104]
+	ldr	r3, [fp, #-116]
 	ldr	r2, [fp, #-44]
 	ldr	r1, [fp, #-40]
 	movw	r0, #:lower16:.LC5
@@ -514,31 +581,22 @@ main:
 	ldr	r3, [fp, #-44]
 	add	r3, r3, #1
 	str	r3, [fp, #-44]
-.L32:
+.L36:
 	ldr	r3, [fp, #-44]
 	cmp	r3, #15
-	ble	.L33
+	ble	.L37
 	ldr	r3, [fp, #-40]
 	add	r3, r3, #1
 	str	r3, [fp, #-40]
-.L31:
+.L35:
 	ldr	r3, [fp, #-40]
 	cmp	r3, #15
-	ble	.L34
+	ble	.L38
 	mov	r3, #0
 	mov	r0, r3
 	sub	sp, fp, #12
 	@ sp needed
 	pop	{r4, r5, fp, pc}
-.L37:
-	.align	3
-.L36:
-	.word	0
-	.word	1
-	.word	2
-	.word	3
-	.word	0
-	.word	1093567616
 	.size	main, .-main
 	.ident	"GCC: (GNU) 8.2.1 20180801 (Red Hat 8.2.1-2)"
 	.section	.note.GNU-stack,"",%progbits
