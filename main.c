d0f58c6d61d776c0c123e7881a1b6beb0a2f68eb
TyRicard <tricard.summer@gmail.com>
2022-08-11 18:00:29 -0700

    Moving timer position for non-optimized

--- main.c
+++ main.c
@@ -231,6 +231,10 @@ int main(int argc, char *argv[])
         }
     }
 
+    clock_t end = clock();
+    time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("The elapsed time is %f seconds\n", time_spent);
+
     for (int32_t i = 0; i < NUMBLOCKS; ++i)
     {
         for (int32_t j = 0; j < NUMBLOCKS; ++j)
@@ -242,8 +246,5 @@ int main(int argc, char *argv[])
         }
     }
     
-    clock_t end = clock();
-    time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
-    printf("The elapsed time is %f seconds", time_spent);
     return 0;
 }
